d167de5531011af2bd2b61b1fa22f725d9482fb7
Juliano J. S. Tavares <julianojstavares@outlook.com>
2023-07-06 18:31:22 -0300

    Section 4, class 92

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_app/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,21 +10,79 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final _transactions = [
+    Transaction(
+      id: "t1",
+      title: "Novo tênis de corrida",
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Conta de luz",
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Despesas Pessoais"),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text("Gráfico"),
+              ),
+            ),
+            Column(
+              children: _transactions.map((t) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(t.value.toString()),
+                      ),
+                      Column(
+                        children: [
+                          Text(t.title),
+                          Text(
+                            t.date.toString(),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ));
   }
 }
